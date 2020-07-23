ffebcd998263193f72aa71260e4f24c7619c4d50
ChoiBU <cbw0916@gmail.com>
2020-07-23 09:01:58 +0900

    Update DB schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,31 +1,32 @@
 CREATE TABLE oauth_clients (
-  id                INT             NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  client_id         VARCHAR(86),
+  client_id         VARCHAR(86)     NOT NULL PRIMARY KEY,
   client_secret     VARCHAR(32),
   redirect_uri      VARCHAR(500),
   grant_type        VARCHAR(20),
-  username          VARCHAR(40),
-  PRIMARY KEY (client_id)
 );
 
 CREATE TABLE oauth_authorization_codes (
   code              VARCHAR(86)     NOT NULL PRIMARY KEY,
   client_id         VARCHAR(32)     NOT NULL,
-  username          VARCHAR(40),
+  username          VARCHAR(40)     NOT NULL,
   redirect_uri      VARCHAR(500),
-  expires           TIMESTAMP       NOT NULL,
+  expired_at        TIMESTAMP       NOT NULL,
+  created_at        TIMESTAMP       NOT NULL,
 );
 
 CREATE TABLE oauth_access_tokens (
-  token             VARCHAR(400)     NOT NULL PRIMARY KEY,
+  token             VARCHAR(400)    NOT NULL PRIMARY KEY,
   client_id         VARCHAR(32)     NOT NULL,
-  username          VARCHAR(40),
-  expires           TIMESTAMP       NOT NULL,
+  username          VARCHAR(40)     NOT NULL,
+  expired_at        TIMESTAMP       NOT NULL,
+  created_at        TIMESTAMP       NOT NULL,
 );
 
 CREATE TABLE oauth_refresh_tokens (
-  token             VARCHAR(400)     NOT NULL PRIMARY KEY,
+  token             VARCHAR(400)    NOT NULL PRIMARY KEY,
   client_id         VARCHAR(32)     NOT NULL,
-  username          VARCHAR(40),
-  expires           TIMESTAMP       NOT NULL,
+  username          VARCHAR(40)     NOT NULL,
+  access_token      VARCHAR(400),
+  expired_at        TIMESTAMP       NOT NULL,
+  created_at        TIMESTAMP       NOT NULL,
 );
