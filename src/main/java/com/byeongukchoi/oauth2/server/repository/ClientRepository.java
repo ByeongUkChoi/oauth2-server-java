7a95a9b51dc72e0d61cad1b47eebe8a6a7b89114
ChoiBU <cbw0916@gmail.com>
2020-08-05 18:52:35 +0900

    Fix ClientRepository

--- src/main/java/com/byeongukchoi/oauth2/server/repository/ClientRepository.java
+++ src/main/java/com/byeongukchoi/oauth2/server/repository/ClientRepository.java
@@ -1,10 +1,7 @@
 package com.byeongukchoi.oauth2.server.repository;
 
-import com.byeongukchoi.oauth2.server.entity.Client;
-
 /**
  * ClientRepository interface
  */
 public interface ClientRepository {
-    public Client findById(String clientId);
 }
