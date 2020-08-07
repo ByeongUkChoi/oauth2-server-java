455b9be577efd95ea9e44deec30aac1dbada3119
ChoiBU <cbw0916@gmail.com>
2020-08-08 00:24:20 +0900

    Fix AuthorizationCodeRepository

--- src/main/java/com/byeongukchoi/oauth2/server/repository/AuthorizationCodeRepository.java
+++ src/main/java/com/byeongukchoi/oauth2/server/repository/AuthorizationCodeRepository.java
@@ -6,7 +6,7 @@ import com.byeongukchoi.oauth2.server.entity.AuthorizationCode;
  * AuthorizationCodeRepository interface
  */
 public interface AuthorizationCodeRepository {
-    public AuthorizationCode getNewCode();
+    public AuthorizationCode getNewCode(String clientId, String username, String redirectUri);
     public AuthorizationCode findByCodeAndClientId(String code, String clientId);
     public void save(AuthorizationCode authorizationCode);
 }
