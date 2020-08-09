f5f23697cfae240656a24eb5d95b24828c0b8aba
ChoiBU <cbw0916@gmail.com>
2020-08-09 22:53:10 +0900

    Add Generic

--- src/main/java/com/byeongukchoi/oauth2/server/repository/AuthorizationCodeRepository.java
+++ src/main/java/com/byeongukchoi/oauth2/server/repository/AuthorizationCodeRepository.java
@@ -6,6 +6,6 @@ package com.byeongukchoi.oauth2.server.repository;
 public interface AuthorizationCodeRepository<T, ID> {
     public T getNewCode(String clientId, String username, String redirectUri);
     public T findByCodeAndClientId(String code, String clientId);
-    public void save(T authorizationCode);
+    public T save(T authorizationCode);
     public void expireCode(T authorizationCode);
 }
