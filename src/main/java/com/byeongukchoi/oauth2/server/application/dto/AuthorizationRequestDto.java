05e7227ae4cc6947dceee3878ff258617c5cc7d9
ByeongUk Choi <cbw0916@gmail.com>
2021-07-23 18:32:22 +0900

    Add AuthorizationRequestDto builder

--- src/main/java/com/byeongukchoi/oauth2/server/application/dto/AuthorizationRequestDto.java
+++ src/main/java/com/byeongukchoi/oauth2/server/application/dto/AuthorizationRequestDto.java
@@ -1,6 +1,8 @@
 package com.byeongukchoi.oauth2.server.application.dto;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 /**
@@ -16,4 +18,26 @@ public class AuthorizationRequestDto {
     private String code;
     private String clientSecret;
     private String refreshToken;
+
+    @Builder
+    public AuthorizationRequestDto(@NonNull String clientId, @NonNull String redirectUri, @NonNull String username) {
+        this.clientId = clientId;
+        this.redirectUri = redirectUri;
+        this.username = username;
+    }
+
+    @Builder
+    public AuthorizationRequestDto(@NonNull String grantType,
+                                   @NonNull String clientId,
+                                   @NonNull String redirectUri,
+                                   @NonNull String code,
+                                   @NonNull String refreshToken,
+                                   @NonNull String clientSecret) {
+        this.grantType = grantType;
+        this.clientId = clientId;
+        this.redirectUri = redirectUri;
+        this.code = code;
+        this.refreshToken = refreshToken;
+        this.clientSecret = clientSecret;
+    }
 }
